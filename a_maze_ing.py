0f73d2be4c1ede2bbb605de61bbee698292f3482
dmytro <dmytrogeo@gmail.com>
2026-09-01 12:43:34 +0100

    add MazeParsing class and move parsing functions in config_parser.py into the class

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from config_parser import parse_config_from_file
+from config_parser import MazeParsing
 from mazegen.errors import MazeConfigError
 from mazegen.maze import Maze
 from parser_errors import MazeParserError
 import sys
+
 
 
 def main() -> int:
@@ -14,7 +15,7 @@ def main() -> int:
               file=sys.stderr)
         return 1
     try:
-        maze_config = parse_config_from_file(sys.argv[1])
+        maze_config = MazeParsing.parse_config_from_file(sys.argv[1])
     except (MazeConfigError, MazeParserError, ValueError, OSError) as e:
         print(f"Error: {e}", file=sys.stderr)
         return 1
